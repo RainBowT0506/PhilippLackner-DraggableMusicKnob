3e105e2ac9693edc5db99e400386d7672fd4a57a
RainBowT0506 <rainbowt0506@gmail.com>
2024-06-28 17:44:16 +0800

    feat: Implement customizable music knob UI component with rotation functionality
    - Added a new composable function `MusicKnob` to create a customizable music knob UI component.
    - Integrated Canvas to enable drawing and touch event handling for knob rotation.
    - Utilized trigonometry to calculate the rotation angle based on touch input.
    - Ensured smooth rotation by converting radians to degrees and managing angle boundaries.
    - Included functionality to emit a percentage value reflecting the knob's rotation.
    - Configured rotation limits to prevent infinite rotation.
    - Applied state management to track touch positions and knob center coordinates.
    - Utilized `pointerInteropFilter` to handle touch events and calculate the rotation angle.
    - Implemented global positioning to determine the knob's center within the screen.
    - Ensured the rotation transformation is applied sequentially to maintain accurate touch coordinates.

--- app/src/main/java/com/rainbowt/draggablemusicknob/MainActivity.kt
+++ app/src/main/java/com/rainbowt/draggablemusicknob/MainActivity.kt
@@ -1,46 +1,100 @@
 package com.rainbowt.draggablemusicknob
 
 import android.os.Bundle
+import android.view.MotionEvent
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.rainbowt.draggablemusicknob.ui.theme.PhilippLacknerDraggableMusicKnobTheme
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.input.pointer.pointerInteropFilter
+import androidx.compose.ui.layout.boundsInWindow
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.res.painterResource
+import kotlin.math.PI
+import kotlin.math.atan2
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            PhilippLacknerDraggableMusicKnobTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+
         }
     }
 }
 
+@OptIn(ExperimentalComposeUiApi::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun MusicKnob(
+    modifier: Modifier = Modifier,
+    limitingAngle: Float = 25f,
+    onValueChange: (Float) -> Unit
+) {
+    var rotation by remember {
+        mutableStateOf(limitingAngle)
+    }
+
+    var touchX by remember {
+        mutableStateOf(0f)
+    }
+    var touchY by remember {
+        mutableStateOf(0f)
+    }
+    var centerX by remember {
+        mutableStateOf(0f)
+    }
+    var centerY by remember {
+        mutableStateOf(0f)
+    }
+
+    Image(
+        painter = painterResource(id = R.drawable.ic_music_knob),
+        contentDescription = "Music Knob",
         modifier = modifier
+            .fillMaxSize()
+            .onGloballyPositioned {
+                val windowBounds = it.boundsInWindow()
+                centerX = windowBounds.size.width / 2f
+                centerY = windowBounds.size.height / 2f
+            }
+            .pointerInteropFilter { event ->
+                touchX = event.x
+                touchY = event.y
+
+                val angle = -atan2(centerX - touchX, centerY - touchY) * (180f / PI).toFloat()
+
+                when (event.action) {
+                    MotionEvent.ACTION_DOWN,
+                    MotionEvent.ACTION_MOVE -> {
+                        if (angle !in -limitingAngle..limitingAngle) {
+                            val fixedAngle =
+                                if (angle in -180f..-limitingAngle) {
+                                    360f + angle
+                                } else {
+                                    angle
+                                }
+                            rotation = fixedAngle
+
+                            val percent = (fixedAngle - limitingAngle) / (360f - 2 * limitingAngle)
+                            onValueChange(percent)
+                            true
+                        } else false
+                    }
+
+                    else -> false
+                }
+            }
+            .rotate(rotation)
     )
 }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    PhilippLacknerDraggableMusicKnobTheme {
-        Greeting("Android")
-    }
-}
